b1e48f89af2476c01e498d3189e2353e754a39c4
upq1 <salmangamer2016@hotmail.com>
2023-10-23 23:23:35 +0300

    waiting saif

---
+++ index.js
@@ -0,0 +1,14 @@
+const imgContainer = document.getElementById("image1")
+
+
+let hue = 0;    
+
+function chgColor() {
+    hue = (hue + 1) % 360;
+    const color = `linear-gradient(0deg, hsl(${hue}, 100%, 50%), hsl(${(hue + 60) % 360}, 100%, 50%) `
+    imgContainer.style.backgroundImage = color 
+}
+
+setInterval(chgColor, 20)
+
+
