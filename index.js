2ea87757c8af44c185d325f1c45bafd7bf375f19
upq1 <salmangamer2016@hotmail.com>
2023-10-24 11:41:13 +0300

    make rgb prettier

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const imgContainer = document.getElementById("imageContainer")
 
-
+let deg = 0;
 let hue = 0;    
 
 function chgColor() {
     hue = (hue + 1) % 360;
-    const color = `linear-gradient(0deg, hsl(${hue}, 100%, 50%), hsl(${(hue + 60) % 360}, 100%, 50%) `
+    deg = (deg + .1) % 360;
+    const color = `linear-gradient(${deg}deg, hsl(${hue}, 100%, 50%), hsl(${(hue + 60) % 360}, 100%, 50%) `
     imgContainer.style.backgroundImage = color 
 }
 
